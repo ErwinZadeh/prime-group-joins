2026506b2771c4d4a57dad979ac0de7856310bd3
ErwinZadeh <farshidmzadeh@icloud.com>
2020-03-03 13:47:10 -0600

    Questiones were added to .sql file

--- joins-solution.sql
+++ joins-solution.sql
@@ -1,1 +1,24 @@
+--Tasks:
+
+--Get all customers and their addresses.
+SELECT * FROM "customers"
+JOIN "addresses" ON "customers"."id" = "addresses"."customer_id"
+
+--Get all orders and their line items (orders, quantity and product).
+
+--Which warehouses have cheetos?
+--Which warehouses have diet pepsi?
+--Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
+--How many customers do we have?
+--How many products do we carry?
+--What is the total available on-hand quantity of diet pepsi?
+
+
+--Stretch:
+
+--How much was the total cost for each order?
+--How much has each customer spent in total?
+--How much has each customer spent in total? Customers who have spent $0 should still show up in the table. It should say 0, not NULL (research coalesce).
+
+
 
